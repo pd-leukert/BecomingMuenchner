01e834d0df1c29845de5a818ef8bcd0709430c84
dabund24 <dev@dabund24.de>
2025-11-22 13:39:40 +0100

    frontend initial commit

---
+++ frontend/src/routes/[pageId]/CheckPage.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import type { CheckPageContent, MockPageContent } from '$lib/types';
+
+	type Props = {
+		pageContent: CheckPageContent;
+	};
+
+	const { pageContent }: Props = $props();
+</script>
+
+<h1>Überprüfung der Angaben</h1>
+
+<p>Lasse deine Angaben überprüfen, bevor du sie endgültig einreichst.</p>
+
+<button class="btn">Überprüfen</button>
