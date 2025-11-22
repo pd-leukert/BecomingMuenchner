45e4386dc8c1ab1a29c9590dccfe540867ebcf5d
pd-leukert <45048954+pd-leukert@users.noreply.github.com>
2025-11-22 15:47:35 +0100

    Add database backend server hosting

--- database/app/database.py
+++ database/app/database.py
@@ -8,7 +8,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 load_dotenv()
 
 INSTANCE_CONNECTION_NAME = os.getenv("INSTANCE_CONNECTION_NAME")
-DB_HOST = os.getenv("DB_HOST")
+#DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT", "3306")
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
