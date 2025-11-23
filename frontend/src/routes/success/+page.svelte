378dea45fe199409ef9654a48ca10d62968019b5
dabund24 <dev@dabund24.de>
2025-11-23 02:05:58 +0100

    typo

--- frontend/src/routes/success/+page.svelte
+++ frontend/src/routes/success/+page.svelte
@@ -6,4 +6,4 @@
 
 <CircleCheck class="h-20 w-20 mx-auto my-8" />
 
-<p>Your submission as submitted and will be processed. We will notify you about further updates.</p>
+<p>Your submission was submitted and will be processed. We will notify you about further updates.</p>
